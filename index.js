e0f32cfb32e1f844647c0d1dec54d57ce12cf064
abdul kabeer khan <162288997+kabeer129@users.noreply.github.com>
2024-03-30 19:51:23 +0500

    push my atm machine

---
+++ index.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = 10000;
+let mypin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === mypin) {
+    console.log("correct pin code!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance", "deposite"]
+        }
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        mybalance -= amountAns.amount;
+        console.log("your remaining balance is:" + mybalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is:" + mybalance);
+    }
+    else if (operationAns.operation === "deposite") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your deposite amount?",
+                type: "number"
+            }
+        ]);
+        mybalance += amountAns.amount;
+        console.log("your new balance is:" + mybalance);
+    }
+}
+else {
+    console.log("incorrect pin code");
+}
